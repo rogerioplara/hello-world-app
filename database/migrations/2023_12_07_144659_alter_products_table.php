dad5c4ea0d3198b8077318479d998d88bb4cfb31
Rogerio pc Interativa <rogerioo.lara@hotmail.com>
2023-12-07 11:59:44 -0300

    migrations, rollback e alter_table

---
+++ database/migrations/2023_12_07_144659_alter_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        /*
+        Para fazer a alteração em uma tabela que já existe, é necessário fazer:
+        Criar o schema da tabela em uma nova migration, geralmente nominada alter_nomeTabela_table
+        Criar a função Schema::table e inserir os dados normalmente como se fosse uma nova migration normal
+        escolher com o after(); o local da coluna
+        
+        dar o dropcolumn nessa mesma coluna que está criando no down()
+
+        Dessa forma é criado um "versionamento" do banco de dados, onde é possível dar rollback utilizando as migrations sem afetar os dados
+        */
+        Schema::table('products', function (Blueprint $table) {
+            $table->integer('min_quantity')
+                ->default(0)
+                ->after('quantity');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // Fazer a exclusão da coluna
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('min_quantity');
+        });
+    }
+};
